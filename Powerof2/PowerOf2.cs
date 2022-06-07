b07dc1fce3bc75ce17b12af6f24d395f47ca8a80
RupaSappati <rupasappati@gmail.com>
2022-06-07 09:53:51 -0700

    Added: Power of two

---
+++ Powerof2/PowerOf2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Powerof2
+{
+    public class PowerOf2
+    {
+    public void PowerOfTwo()
+        {
+            int N;
+            int baseNum = 0;
+           
+
+            Console.WriteLine("Enter a Expoent : ");
+            N=Convert.ToInt32(Console.ReadLine());
+
+            //condition to giving the range of 1 to 32
+
+            if(0 <= N && N <= 31)
+            {
+                for(int i = 0; i < N; i++)
+                {
+                    if (i == 0)
+                    {
+                        baseNum = 1;
+                    }
+                    else
+                    {
+                        baseNum = baseNum * 2;
+                    }
+                    Console.WriteLine("2^{0} ={1}", i,baseNum);
+                }
+            }
+        }
+
+    }
+}
---
+++ Powerof2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Powerof2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            PowerOf2 obj = new PowerOf2();
+            obj.PowerOfTwo();
+        }
+    }
+}
